a50060880d52e2723be8e8dd6b1ea322fcf82a18
Hrishikesh Bagalkote <hbagalkote@gmail.com>
2026-02-14 17:14:37 -0800

    rpc listener + url store rpc headers

---
+++ lib/rpc_listener.h
@@ -0,0 +1,68 @@
+#pragma once
+
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <cstdint>
+#include <cstddef>
+#include <functional>
+
+
+class RPCListener {
+public:
+
+    // Constructor: creates and binds a listening socket on the given port
+    RPCListener(uint16_t port) : listen_fd(-1) {
+        listen_fd = socket(AF_INET, SOCK_STREAM, 0);
+        if (listen_fd < 0) return;
+
+        int opt = 1;
+        setsockopt(listen_fd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt));
+
+        struct sockaddr_in addr{};
+        addr.sin_family = AF_INET;
+        addr.sin_addr.s_addr = INADDR_ANY;
+        addr.sin_port = htons(port);
+
+        if (bind(listen_fd, (struct sockaddr*)&addr, sizeof(addr)) < 0) {
+            close(listen_fd);
+            listen_fd = -1;
+            return;
+        }
+
+        if (listen(listen_fd, SOMAXCONN) < 0) {
+            close(listen_fd);
+            listen_fd = -1;
+        }
+    }
+
+
+    // Destructor: close the listening socket
+    ~RPCListener() {
+        if (listen_fd >= 0) close(listen_fd);
+    }
+
+
+    // Listener loop: blocks forever, calling handler with each new client fd.
+    // The handler is responsible for reading/writing to the fd and closing it when done.
+    void listener_loop(std::function<void(int)> handler) {
+        while (true) {
+            struct sockaddr_in client_addr{};
+            socklen_t client_len = sizeof(client_addr);
+
+            int client_fd = accept(listen_fd, (struct sockaddr*)&client_addr, &client_len);
+            if (client_fd < 0) continue;
+
+            // TODO(hershey): multithread this via thread pool mechanism?
+            handler(client_fd);
+        }
+    }
+
+
+    // Interface: check for listen socket validity before calling the accept loop
+    bool valid() const { return listen_fd >= 0; }
+
+
+private:
+    int listen_fd;
+};
---
+++ lib/rpc_urlstore.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#include "string.h"
+#include "vector.h"
+#include <cstdint>
+
+// Request sent to end server hosting the (sharded) dynamic URL data when a new URL is encountered
+// This request is capable of being batched (per URL)
+// Distance from seed list is never something that needs to be updated, so it is not included in this RPC
+struct URLStoreUpdateRequest {
+    string url;                         // URL: primary identifier
+    vector<string> anchor_text;         // Vector of anchor text strings used to refer to this URL since its last update (potentially size >1)
+    uint32_t num_encountered;           // Number of additional times this URL has been encountered since its last update
+};
