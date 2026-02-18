4c5147b5d3bde1eabbc3c11c4933f2f4df4b1665
charlesthuang <chuang6703@gmail.com>
2026-02-18 03:06:12 -0500

    complete draft impl. of url_store logic

---
+++ url_store/url_store.cpp
@@ -0,0 +1,95 @@
+#include "url_store.h"
+
+
+const UrlData* UrlStore::findUrlData(const string& url) const {
+    const Slot<string, UrlData>* slot = url_data.find(url);
+    return slot ? &slot->value : nullptr;
+}
+
+UrlData* UrlStore::findUrlData(const string& url) {
+    Slot<string, UrlData>* slot = url_data.find(url);
+    return slot ? &slot->value : nullptr;
+}
+
+/*
+Persisted at same time as index chunks
+
+Stored in url_store_<worker #>.txt
+Vector of anchor_texts (variable length) seen to id mapping (index)
+For each URL:
+    <url>\n
+    Metadata: <times seen (32 bits)> <distance from seed list (16 bits)> <end of title (16 bits)> <end of description (16 bits)>\n
+    Anchor text list.
+        For each list: <anchor_text id (32 bits)> <times seen (32 bits)>\n
+*/
+void UrlStore::persist() {
+    // given the current data and worker this urlstore is assigned to
+    // persist to provided file
+    FILE* fd = fopen("urlstore_" + string::to_string(WORKER_NUMBER) + ".txt", "wx");
+
+    if (fd == nullptr) perror("Error opening urlstore file for writing.");
+
+    fprintf(fd, "%u", anchor_to_id.size());
+    for (const string& anchor_text : anchor_to_id) {
+        fprintf(fd, "%lu", anchor_text.size());
+        fwrite(&anchor_text, sizeof(char), anchor_text.size(), fd);
+        fwrite("\n", sizeof(char), 1, fd);
+    }
+    
+    for (const auto& [url, data] : url_data) {
+        fprintf(fd, "%lu", url.size());
+        fwrite(&url, sizeof(char), url.size(), fd);
+        fprintf(fd, "%u %u %u %u %u\n", data.num_encountered, data.seed_distance, data.eot, data.eod, data.anchor_freqs.size());
+
+        for (const auto& anchor_freq : data.anchor_freqs) {
+            fprintf(fd, "%u %u\n", anchor_freq.anchor_id, anchor_freq.freq);
+        }
+    }
+}
+
+void UrlStore::readFromFile(UrlStore& url_store, const int worker_number) {
+    // given a urlstore object and worker number, read from corresponding file and update urlstore object accordingly
+    FILE* fd = fopen("urlstore_" + string::to_string(worker_number) + ".txt", "r");
+
+    if (fd == nullptr) perror("Error opening urlstore file for reading.");
+
+    uint32_t num_anchor_texts;
+    char dummy;
+    fread(&num_anchor_texts, sizeof(uint32_t), 1, fd);
+    for (uint32_t i = 0; i < num_anchor_texts; i++) {
+        uint32_t anchor_text_len;
+        fread(&anchor_text_len, sizeof(uint32_t), 1, fd);
+        string anchor_text(anchor_text_len, '\0');
+        fread(&anchor_text[0], sizeof(char), anchor_text_len, fd);
+        url_store.anchor_to_id.push_back(anchor_text);
+        fread(&dummy, sizeof(char), 1, fd); // consume newline
+    }
+
+    uint32_t url_len;
+    while (fread(&url_len, sizeof(uint32_t), 1, fd)) {
+        string url(url_len, '\0');
+        fread(&url[0], sizeof(char), url_len, fd);
+        url_store.url_data[url] = UrlData();
+
+        uint32_t num_encountered, num_anchor_freqs;
+        uint16_t seed_distance, eot, eod;
+        fread(&num_encountered, sizeof(uint32_t), 1, fd);
+        fread(&seed_distance, sizeof(uint16_t), 1, fd);
+        fread(&eot, sizeof(uint16_t), 1, fd);
+        fread(&eod, sizeof(uint16_t), 1, fd);
+        fread(&num_anchor_freqs, sizeof(uint32_t), 1, fd);
+        url_store.url_data[url].num_encountered = num_encountered;
+        url_store.url_data[url].seed_distance = seed_distance;
+        url_store.url_data[url].eot = eot;
+        url_store.url_data[url].eod = eod;
+
+        for (uint32_t i = 0; i < num_anchor_freqs; i++) {
+            uint32_t anchor_id, freq;
+            fread(&anchor_id, sizeof(uint32_t), 1, fd);
+            fread(&freq, sizeof(uint32_t), 1, fd);
+            url_store.url_data[url].anchor_freqs.push_back({anchor_id, freq});
+        }
+    }
+
+    fclose(fd);
+}
---
+++ url_store/url_store.h
@@ -0,0 +1,80 @@
+#include "lib/unordered_map.h"
+#include "lib/vector.h"
+#include "lib/string.h"
+
+struct AnchorData {
+    uint32_t anchor_id;
+    uint32_t freq;
+};
+
+struct UserAnchorData {
+    const string* anchor_text;
+    uint32_t freq;
+};
+
+// TODO: decide if vector is better than hashmap for anchor_freqs (cache locality vs. O(1) average lookup)
+struct UrlData {
+    vector<AnchorData> anchor_freqs;                     // anchor text id to frequency since its last update (potentially size >1)
+    uint32_t num_encountered;                            // Number of additional times this URL has been
+    uint16_t seed_distance;                              // Distance from seed list (never needs to be updated, so it is not included in the RPC)
+    uint16_t eot;                                        // End of title
+    uint16_t eod;                                        // End of description
+};
+
+class UrlStore {
+private:
+    unordered_map<string, UrlData> url_data;
+    vector<string> anchor_to_id; // anchor text to corresponding id (index)
+
+    const UrlData* findUrlData(const string& url) const;
+    UrlData* findUrlData(const string& url);
+
+public:
+    UrlStore() {};
+    void persist();
+
+    // to read urlStore from disk after a crash, each worker thread will read from its corresponding files and update it's urlstore object accordingly
+    static void readFromFile(UrlStore& url_store, const int worker_number);
+
+    bool addUrl(const string& url, const vector<string>& anchor_texts, const uint16_t seed_distance, const uint16_t eot, const uint16_t eod, const uint32_t num_encountered);
+    bool updateUrl(const string& url, const vector<string>& anchor_texts, const uint32_t num_encountered);
+
+    vector<UserAnchorData> getUrlAnchorInfo(const string& url) {
+        const UrlData* it = findUrlData(url);
+        if (it == nullptr) return {};
+        if (it->anchor_freqs.empty()) return {};
+
+        vector<UserAnchorData> user_anchor_data;
+        user_anchor_data.reserve(it->anchor_freqs.size());
+        for (const AnchorData& anchor_freq : it->anchor_freqs) {
+            user_anchor_data.push_back({&anchor_to_id[anchor_freq.anchor_id] , anchor_freq.freq});
+        }
+
+        return user_anchor_data;
+    }
+
+    uint32_t getUrlNumEncountered(const string& url) {
+        const UrlData* it = findUrlData(url);
+        if (it == nullptr) return 0;
+        return it->num_encountered;
+    }
+
+    uint16_t getUrlSeedDistance(const string& url) {
+        const UrlData* it = findUrlData(url);
+        return it ? it->seed_distance : UINT16_MAX;
+    }
+
+    bool inTitle(const string& url, uint16_t word_pos) {
+        const UrlData* it = findUrlData(url);
+        return it ? word_pos < it->eot : false;
+    }
+
+    bool inDescription(const string& url, uint16_t word_pos) {
+        const UrlData* it = findUrlData(url);
+        return it ? it->eot <= word_pos && word_pos < it->eod : false;
+    }
+
+};
+
+// TODO: This needs to be defined globally on bootup (defining here for now)
+const uint32_t WORKER_NUMBER = 0;
