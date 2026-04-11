8c8a6285400373098ad0a210287ad3d3fb6c6a9e
Erik Nielsen <erikvn@umich.edu>
2026-04-11 14:48:05 -0400

    init

--- lib/consts.h
+++ lib/consts.h
@@ -10,8 +10,8 @@ constexpr uint8_t LOG_LEVEL = 3;
 constexpr const char* USER_AGENT = "Seamus the Search Engine (web crawler for university course)";
 
 // Global
-constexpr size_t NUM_MACHINES = 1;
-constexpr const char* MACHINES[NUM_MACHINES] = {"127.0.0.1"};
+constexpr size_t NUM_MACHINES = 3;
+constexpr const char* MACHINES[NUM_MACHINES] = {"34.70.54.47", "136.119.91.226", "35.223.96.82"};
 
 inline const char* get_machine_addr(size_t machine_id) {
     assert(machine_id < NUM_MACHINES);
