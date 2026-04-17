250a4edc10e87dd999e8a90b91656c5e7a8aa32f
Hrishikesh Bagalkote <hbagalkote@gmail.com>
2026-04-17 03:17:31 -0400

    lower cap to 500k

--- lib/consts.h
+++ lib/consts.h
@@ -169,7 +169,7 @@ constexpr const char* SEED_LIST[SEED_LIST_SIZE] = {
 
 // Parser
 /* static constexpr size_t MAX_PARSED_PAGES = (3*1e8)/18; // 300M/18 */
-static constexpr size_t MAX_PARSED_PAGES = 1000000;
+static constexpr size_t MAX_PARSED_PAGES = 500000;
 static constexpr const char* PARSER_OUTPUT_DIR = "/var/seamus/parser_output";
 static constexpr int MAX_CONSECUTIVE_NON_ALNUM = 100;
 static constexpr int MAX_CONSECUTIVE_NON_ENGLISH = 20;
